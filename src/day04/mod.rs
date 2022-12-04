ae1e2bb02142ae4582c349283f7c7d656df328da
Chris Petkau <47264230+chrispetkau@users.noreply.github.com>
2022-12-04 10:07:38 -0800

    Factored parsing out.

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -43,42 +43,31 @@ fn symmetric_contains(a: &Range, b: &Range) -> bool {
             a.from == b.from // b contains a
         }
     } else {
-        if a.to <= b.to {
-            true // b contains a
-        } else {
-            false
-        }
+        a.to <= b.to
     }
 }
 
 fn solve_for(input: &str) -> Result<(usize, usize, Duration)> {
     let timer = Instant::now();
-    let parse_duration = timer.elapsed();
-    let part1 = input
+    let elves = input
         .lines()
-        .filter(|line| {
+        .map(|line| {
             let mut elves = line.split(',').map(|s| s.parse::<Range>());
-            symmetric_contains(
-                &elves.next().unwrap().unwrap(),
-                &elves.next().unwrap().unwrap(),
+            (
+                elves.next().unwrap().unwrap(),
+                elves.next().unwrap().unwrap(),
             )
         })
+        .collect::<Vec<_>>();
+    let parse_duration = timer.elapsed();
+    let part1 = elves
+        .iter()
+        .filter(|(a, b)| symmetric_contains(a, b))
         .count();
-    let part2 = input
-        .lines()
-        .filter(|line| {
-            let mut elves = line.split(',').map(|s| s.parse::<Range>());
-            elves
-                .next()
-                .unwrap()
-                .unwrap()
-                .overlaps(&elves.next().unwrap().unwrap())
-        })
-        .count();
+    let part2 = elves.iter().filter(|(a, b)| a.overlaps(b)).count();
     Ok((part1, part2, parse_duration))
 }
 
-// TODO figure out why this is so much slower than Chris Ozeroff's solution
 pub(crate) fn solve() -> (usize, usize, Duration) {
     match solve_for(INPUT) {
         Ok(answer) => answer,
