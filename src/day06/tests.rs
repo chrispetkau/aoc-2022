65b5d1d54e3d63e41643b56db1045a1ece9889b8
Chris Petkau <47264230+chrispetkau@users.noreply.github.com>
2022-12-05 21:27:37 -0800

    Day 06, part1 solved.

--- src/day06/tests.rs
+++ src/day06/tests.rs
@@ -1,15 +1,16 @@
 use super::{input, solve_for};
 
-const INPUT: [usize; 5] = [3, 4, 3, 1, 2];
-
 #[test]
 fn part1() {
-    assert_eq!(26, solve_for(&INPUT, &[18])[0]);
-    assert_eq!(5934, solve_for(&INPUT, &[80])[0]);
-    assert_eq!(374927, solve_for(&input::INPUT, &[80])[0]);
+    assert_eq!(7, solve_for("mjqjpqmgbljsphdztnvjfqwrcgsmlb").0);
+    assert_eq!(5, solve_for("bvwbjplbgvbhsrlpgdmjqwftvncz").0);
+    assert_eq!(6, solve_for("nppdvjthqldpwncqszvftbrmjlhg").0);
+    assert_eq!(10, solve_for("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").0);
+    assert_eq!(11, solve_for("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").0);
+    assert_eq!(1300, solve_for(input::INPUT).0);
 }
 
 #[test]
 fn part2() {
-    assert_eq!(1687617803407, solve_for(&input::INPUT, &[256])[0]);
+    // assert_eq!(1687617803407, solve_for(&input::INPUT, &[256])[0]);
 }
