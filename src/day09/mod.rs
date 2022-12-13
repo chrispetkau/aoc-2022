be1bd42cb159d06b1624ecf4a4a0de059ce3c314
Chris Petkau <47264230+chrispetkau@users.noreply.github.com>
2022-12-12 21:11:56 -0800

    Day 09, part 1 solved

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -1,7 +1,9 @@
 use self::input::INPUT;
 use std::{
-    cmp::Ordering,
     collections::HashSet,
+    num::ParseIntError,
+    ops::{Add, AddAssign, Sub},
+    str::FromStr,
     time::{Duration, Instant},
 };
 
@@ -10,166 +12,136 @@ mod input;
 #[cfg(test)]
 mod tests;
 
-const LEFT: u8 = 1 << 0;
-const RIGHT: u8 = 1 << 1;
-const UP: u8 = 1 << 2;
-const DOWN: u8 = 1 << 3;
-const ALL: u8 = LEFT | RIGHT | UP | DOWN;
-const TERMINAL: u8 = 1 << 4;
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
 
-fn set_left_right(transitions: &mut [u8], left: usize, right: usize, left_cmp_right: Ordering) {
-    match left_cmp_right {
-        Ordering::Less => transitions[right] |= LEFT,
-        Ordering::Equal => {
-            transitions[right] |= LEFT;
-            transitions[left] |= RIGHT;
-        }
-        Ordering::Greater => transitions[left] |= RIGHT,
+impl Point {
+    fn manhattan_distance(&self) -> usize {
+        self.x.abs() as usize + self.y.abs() as usize
     }
 }
 
-fn set_up_down(transitions: &mut [u8], up: usize, down: usize, up_cmp_down: Ordering) {
-    match up_cmp_down {
-        Ordering::Less => transitions[down] |= UP,
-        Ordering::Equal => {
-            transitions[down] |= UP;
-            transitions[up] |= DOWN;
+impl Add<Direction> for Point {
+    type Output = Self;
+
+    fn add(self, direction: Direction) -> Self::Output {
+        match direction {
+            Direction::Right => Self {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Direction::Left => Self {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Direction::Up => Self {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Direction::Down => Self {
+                x: self.x,
+                y: self.y + 1,
+            },
         }
-        Ordering::Greater => transitions[up] |= DOWN,
     }
 }
 
-struct FloodFiller<'a> {
-    transitions: &'a [u8],
-    stride: usize,
-    closed: HashSet<usize>,
-    open: Vec<usize>,
+impl AddAssign<Direction> for Point {
+    fn add_assign(&mut self, direction: Direction) {
+        match direction {
+            Direction::Right => self.x += 1,
+            Direction::Left => self.x -= 1,
+            Direction::Up => self.y -= 1,
+            Direction::Down => self.y += 1,
+        }
+    }
 }
 
-impl FloodFiller<'_> {
-    fn execute(&mut self, start: usize) -> usize {
-        self.closed.clear();
-        self.open.clear();
-        self.open.push(start);
-        let mut count = 0;
-        while let Some(index) = self.open.pop() {
-            if !self.closed.insert(index) {
-                continue;
-            }
+impl AddAssign for Point {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
 
-            let transition_mask = self.transitions[index];
-            if (transition_mask & TERMINAL) != 0 {
-                continue;
-            }
+impl Sub for Point {
+    type Output = Self;
 
-            count += 1;
-
-            // Invert the transition mask to determine ascending directions in which
-            // to travel *from* the minimum points.
-            let transition_mask = transition_mask ^ ALL;
-
-            if (transition_mask & LEFT) != 0 {
-                self.open.push(index - 1);
-            }
-            if (transition_mask & RIGHT) != 0 {
-                self.open.push(index + 1);
-            }
-            if (transition_mask & UP) != 0 {
-                self.open.push(index - self.stride);
-            }
-            if (transition_mask & DOWN) != 0 {
-                self.open.push(index + self.stride);
-            }
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
         }
-        count
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct Motion {
+    direction: Direction,
+    distance: usize,
+}
+
+impl FromStr for Motion {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut tokens = s.split_whitespace();
+        let direction = match tokens.next().unwrap() {
+            "R" => Direction::Right,
+            "L" => Direction::Left,
+            "U" => Direction::Up,
+            "D" => Direction::Down,
+            _ => panic!(),
+        };
+        let distance = tokens.next().unwrap().parse::<usize>()?;
+        Ok(Self {
+            direction,
+            distance,
+        })
     }
 }
 
 fn solve_for(input: &str) -> (usize, usize, Duration) {
-    let row_count = input.lines().count();
-    let column_count = input.lines().next().unwrap().len();
-    let stride = column_count + 2;
-    let index = |x: usize, y: usize| x + 1 + ((y + 1) * stride);
+    let timer = Instant::now();
+    let motions = input
+        .lines()
+        .map(|line| line.parse::<Motion>().unwrap())
+        .collect::<Vec<_>>();
+    let parse_duration = timer.elapsed();
 
-    // Buffer the height map with extra rows and columns on the edges of the maximum height.
-    const HEIGHT_MAX: u8 = 9;
-    let mut heights = vec![HEIGHT_MAX; (row_count + 2) * stride];
-
-    // Read in the height map data.
-    let parse_start = Instant::now();
-    input.lines().enumerate().for_each(|(y, line)| {
-        line.chars()
-            .enumerate()
-            .for_each(|(x, height)| heights[index(x, y)] = height.to_digit(10).unwrap() as u8);
-    });
-    let parse_duration = Instant::now() - parse_start;
-
-    // Build a buffered array of transitions.
-    let mut transitions: Vec<u8> = vec![0b11111; heights.len()];
-    (1..column_count + 1)
-        .for_each(|x| (1..row_count + 1).for_each(|y| transitions[x + y * stride] = 0));
-
-    // Iterate over all cells except the buffer rows and columns, checking neighbors in all directions.
-    // A direction bit is set if height decreases, or is equal, in that direction.
-    // Thus minimum points will have no bits set.
-    (1..column_count + 1).for_each(|x| {
-        (1..row_count + 1).for_each(|y| {
-            let current = x + y * stride;
-            let height = heights[current];
-
-            let right = current + 1;
-            set_left_right(
-                &mut transitions,
-                current,
-                right,
-                height.cmp(&heights[right]),
-            );
-
-            let left = current - 1;
-            set_left_right(&mut transitions, left, current, heights[left].cmp(&height));
-
-            let down = current + stride;
-            set_up_down(&mut transitions, current, down, height.cmp(&heights[down]));
-
-            let up = current - stride;
-            set_up_down(&mut transitions, up, current, heights[up].cmp(&height));
-
-            if height == HEIGHT_MAX {
-                transitions[current] |= TERMINAL;
+    let mut head = Point { x: 0, y: 0 };
+    let mut tail = head;
+    let mut visited = HashSet::new();
+    visited.insert(tail);
+    motions.iter().for_each(|motion| {
+        (0..motion.distance).for_each(|_| {
+            head += motion.direction;
+            let delta = head - tail;
+            let manhattan_distance = delta.manhattan_distance();
+            let close_enough =
+                manhattan_distance < 2 || (manhattan_distance == 2 && delta.x != 0 && delta.y != 0);
+            if !close_enough {
+                tail += Point {
+                    x: delta.x.clamp(-1, 1),
+                    y: delta.y.clamp(-1, 1),
+                };
+                visited.insert(tail);
             }
         });
     });
 
-    let indices = (0..column_count).flat_map(|x| (0..row_count).map(move |y| index(x, y)));
-
-    let minima = indices
-        .filter_map(|index| {
-            if transitions[index] == 0 {
-                Some((index, (heights[index] + 1) as usize))
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let part1 = minima
-        .iter()
-        .fold(0, |current, (_index, risk_level)| current + risk_level);
-
-    let mut flood_filler = FloodFiller {
-        transitions: &transitions,
-        open: Vec::new(),
-        closed: HashSet::new(),
-        stride,
-    };
-    let mut basin_sizes = minima
-        .iter()
-        .map(|(index, _risk_level)| flood_filler.execute(*index))
-        .collect::<Vec<usize>>();
-    basin_sizes.sort_unstable();
-    let part2 = basin_sizes.iter().rev().take(3).product();
-
-    (part1, part2, parse_duration)
+    (visited.len(), 0, parse_duration)
 }
 
 pub(crate) fn solve() -> (usize, usize, Duration) {
--- src/day09/tests.rs
+++ src/day09/tests.rs
@@ -1,19 +1,22 @@
 use super::{input, solve_for};
 
-const INPUT: &str = "2199943210
-3987894921
-9856789892
-8767896789
-9899965678";
+const INPUT: &str = "R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2";
 
 #[test]
 fn part1() {
-    assert_eq!(15, solve_for(INPUT).0);
-    assert_eq!(491, solve_for(input::INPUT).0);
+    assert_eq!(13, solve_for(INPUT).0);
+    assert_eq!(5981, solve_for(input::INPUT).0);
 }
 
 #[test]
 fn part2() {
-    assert_eq!(1134, solve_for(INPUT).1);
-    assert_eq!(1075536, solve_for(input::INPUT).1);
+    // assert_eq!(1134, solve_for(INPUT).1);
+    // assert_eq!(1075536, solve_for(input::INPUT).1);
 }
