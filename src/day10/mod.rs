c9d84ea3bdbe471f91f2e4e9320abfbbd4834a6a
Chris Petkau <47264230+chrispetkau@users.noreply.github.com>
2022-12-13 23:12:22 -0800

    Solved day 10, part 2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -2,7 +2,6 @@ use self::input::INPUT;
 use anyhow::Result;
 use std::{
     num::ParseIntError,
-    ops::{Add, Sub},
     str::FromStr,
     time::{Duration, Instant},
 };
@@ -73,6 +72,7 @@ impl Cpu {
     }
 
     fn tick(&mut self) -> bool {
+        // End of a cycle
         let instructions = self.instructions.as_ref().unwrap();
         let instruction = &instructions[self.instruction_index];
         if self.elapsed_instruction_cycles == instruction.duration() {
@@ -85,13 +85,16 @@ impl Cpu {
             self.instruction_index += 1;
             self.elapsed_instruction_cycles = 0;
         }
+
+        // Start of a cycle.
         self.elapsed_instruction_cycles += 1;
         self.cycle += 1;
+
         self.instruction_index != instructions.len()
     }
 }
 
-fn solve_for(input: &str) -> Result<(usize, usize, Duration)> {
+fn solve_for(input: &str) -> Result<(usize, String, Duration)> {
     let timer = Instant::now();
     let instructions = input
         .lines()
@@ -99,36 +102,51 @@ fn solve_for(input: &str) -> Result<(usize, usize, Duration)> {
         .collect::<Result<Vec<Instruction>, ParseIntError>>()?;
     let parse_duration = timer.elapsed();
 
+    const CRT_ROW_COUNT: usize = 6;
+    const CRT_COLUMN_COUNT: usize = 40;
+
     let mut cpu = Cpu::default();
     let mut observation_cycle = 20;
     const OBSERVATION_PERIOD: usize = 40;
     let mut signal_strengths = vec![];
+    let mut crt = vec!['.'; CRT_COLUMN_COUNT * CRT_ROW_COUNT];
     cpu.load(instructions);
     loop {
         if cpu.cycle == observation_cycle {
             signal_strengths.push(cpu.signal_strength());
             observation_cycle += OBSERVATION_PERIOD;
         }
-        
+
         // println!("[{}] x = {}, signal = {}", cpu.cycle, cpu.x, cpu.signal_strength());
 
         if !cpu.tick() {
             break;
         }
+
+        let crt_column = (cpu.cycle - 1) % CRT_COLUMN_COUNT;
+        if cpu.x.wrapping_sub(1) <= crt_column && crt_column <= cpu.x.wrapping_add(1) {
+            crt[cpu.cycle - 1] = '#';
+        }
     }
 
     let part1 = signal_strengths.iter().sum();
-    let part2 = 1;
+
+    let mut part2 = String::new();
+    for row in 0..CRT_ROW_COUNT {
+        part2.extend(&crt[row * CRT_COLUMN_COUNT..(row + 1) * CRT_COLUMN_COUNT]);
+        part2.push('\n');
+    }
+    println!("{part2}");
 
     Ok((part1, part2, parse_duration))
 }
 
-pub(crate) fn solve() -> (usize, usize, Duration) {
+pub(crate) fn solve() -> (usize, String, Duration) {
     match solve_for(INPUT) {
         Ok(solution) => solution,
         Err(error) => {
             println!("day 10 error: {}", error);
-            (0, 0, Duration::new(0, 0))
+            (0, "".to_owned(), Duration::new(0, 0))
         }
     }
 }
