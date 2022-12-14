c8d0aa640b79e2d7979b1f9af2cdf99fd71c2080
Chris Petkau <47264230+chrispetkau@users.noreply.github.com>
2022-12-14 09:30:18 -0800

    Day 11 parsing is working

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,6 +1,9 @@
 use self::input::INPUT;
+use anyhow::{anyhow, Result};
 use std::{
-    collections::HashSet,
+    num::ParseIntError,
+    ops::{Deref, DerefMut},
+    str::FromStr,
     time::{Duration, Instant},
 };
 
@@ -9,90 +12,194 @@ mod input;
 #[cfg(test)]
 mod tests;
 
-fn solve_for(input: &str) -> (usize, usize, Duration) {
-    const SIZE: usize = 10;
-    const STRIDE: usize = SIZE + 2;
-    const BUFFERED_SIZE: usize = STRIDE.pow(2);
-    let mut buffered_energy_levels: [u8; BUFFERED_SIZE] = [0; BUFFERED_SIZE];
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Item(usize);
 
-    // Index into buffered_energy_levels for the specified energy_levels coordinate.
-    let index = |x: usize, y: usize| x + 1 + (y + 1) * STRIDE;
-    let indices = (0..SIZE).flat_map(|y| (0..SIZE).map(move |x| index(x, y)));
+impl Deref for Item {
+    type Target = usize;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
-    // Parse input data into buffered_energy_levels.
-    let parse_start = Instant::now();
-    let input = input
-        .lines()
-        .flat_map(|line| line.chars().map(|c| c.to_digit(10).unwrap() as u8))
-        .collect::<Vec<u8>>();
-    let parse_duration = Instant::now() - parse_start;
+impl DerefMut for Item {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
 
-    indices
-        .clone()
-        .zip(input.iter())
-        .for_each(|(i, &initial_energy_level)| buffered_energy_levels[i] = initial_energy_level);
+impl FromStr for Item {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(s.parse::<usize>()?))
+    }
+}
 
-    const STRIDE_AS_I32: i32 = STRIDE as i32;
-    const NEIGHBOR_OFFSETS: [i32; 8] = [
-        -STRIDE_AS_I32 - 1,
-        -STRIDE_AS_I32,
-        1 - STRIDE_AS_I32,
-        -1,
-        1,
-        STRIDE_AS_I32 - 1,
-        STRIDE_AS_I32,
-        STRIDE_AS_I32 + 1,
-    ];
-    let mut flashed = HashSet::new();
-    let mut sim = || {
-        indices.clone().for_each(|i| buffered_energy_levels[i] += 1);
-        flashed.clear();
-        let flash_count = loop {
-            let flashers = indices
-                .clone()
-                .filter(|&i| 9 < buffered_energy_levels[i])
-                .filter(|&i| flashed.insert(i))
-                .collect::<Vec<usize>>();
-            if flashers.is_empty() {
-                break flashed.len();
-            }
-            flashers.iter().for_each(|&i| {
-                NEIGHBOR_OFFSETS.iter().for_each(|&offset| {
-                    let neighbor_index = (i as i32 + offset) as usize;
-                    buffered_energy_levels[neighbor_index] += 1
-                });
-            });
-        };
-        flashed.iter().for_each(|&i| buffered_energy_levels[i] = 0);
-        flash_count
-    };
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Worry(usize);
 
-    let mut part1 = 0;
-    let mut part2 = None;
-    let mut sim_count = 0;
-    const PART1_SIM_COUNT: usize = 100;
-    const OCTOPUS_COUNT: usize = SIZE.pow(2);
-    loop {
-        let done_part1 = PART1_SIM_COUNT <= sim_count;
-        let flash_count = sim();
-        sim_count += 1;
-        if !done_part1 {
-            part1 += flash_count;
-            if part2.is_some() {
-                break;
-            }
-        }
-        if flash_count == OCTOPUS_COUNT && part2.is_none() {
-            part2 = Some(sim_count);
-            if done_part1 {
-                break;
-            }
+impl Deref for Worry {
+    type Target = usize;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Worry {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl FromStr for Worry {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(s.parse::<usize>()?))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct MonkeyIndex(usize);
+
+impl Deref for MonkeyIndex {
+    type Target = usize;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for MonkeyIndex {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl FromStr for MonkeyIndex {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(s.parse::<usize>()?))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Operation {
+    Add(Worry),
+    Mul(Worry),
+    Square,
+}
+
+impl FromStr for Operation {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut tokens = s
+            .split(':')
+            .nth(1)
+            .unwrap()
+            .split_whitespace()
+            .skip(3);
+        match tokens.next().unwrap() {
+            "+" => Ok(Self::Add(tokens.next().unwrap().parse::<Worry>()?)),
+            "*" => Ok(if let Ok(worry) = tokens.next().unwrap().parse::<Worry>() {
+                Self::Mul(worry)
+            } else {
+                Self::Square
+            }),
+            unhandled_op => Err(anyhow!("Unhandled op {unhandled_op}")),
         }
     }
+}
 
-    (part1, part2.unwrap(), parse_duration)
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Test {
+    divisible_by: usize,
+    if_true: MonkeyIndex,
+    if_false: MonkeyIndex,
+}
+
+impl FromStr for Test {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+        let divisible_by = lines
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<usize>()?;
+        let if_true = lines
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<MonkeyIndex>()?;
+        let if_false = lines
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<MonkeyIndex>()?;
+        Ok(Self {
+            divisible_by,
+            if_true,
+            if_false,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Monkey {
+    items: Vec<Item>,
+    operation: Operation,
+    test: Test,
+}
+
+impl FromStr for Monkey {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines().skip(1);
+        let items = lines
+            .next()
+            .unwrap()
+            .split(':')
+            .nth(1)
+            .unwrap()
+            .split(',')
+            .map(|item| item.trim().parse::<Item>())
+            .collect::<Result<Vec<Item>, ParseIntError>>()?;
+        let operation = lines.next().unwrap().parse::<Operation>()?;
+        let mut test_text = String::new();
+        (0..3).for_each(|_| {
+            test_text.push_str(lines.next().unwrap());
+            test_text.push('\n');
+        });
+        let test = test_text.parse::<Test>()?;
+        Ok(Self {
+            items,
+            operation,
+            test,
+        })
+    }
+}
+
+fn solve_for(input: &str) -> Result<(usize, usize, Duration)> {
+    let timer = Instant::now();
+    let monkeys = input
+        .split("\n\n")
+        .map(|monkey| monkey.parse::<Monkey>())
+        .collect::<Result<Vec<Monkey>>>()?;
+    let parse_duration = timer.elapsed();
+
+    println!("{monkeys:#?}");
+
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    Ok((part1, part2, parse_duration))
 }
 
 pub(crate) fn solve() -> (usize, usize, Duration) {
-    solve_for(INPUT)
+    solve_for(INPUT).unwrap()
 }
--- src/day11/tests.rs
+++ src/day11/tests.rs
@@ -1,24 +1,41 @@
 use super::{input, solve_for};
 
-const INPUT: &str = "5483143223
-2745854711
-5264556173
-6141336146
-6357385478
-4167524645
-2176841721
-6882881134
-4846848554
-5283751526";
+const INPUT: &str = "Monkey 0:
+Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1";
 
 #[test]
 fn part1() {
-    assert_eq!(1656, solve_for(INPUT).0);
-    assert_eq!(1741, solve_for(input::INPUT).0);
+    assert_eq!(10605, solve_for(INPUT).unwrap().0);
+    // assert_eq!(1741, solve_for(input::INPUT).0);
 }
 
 #[test]
 fn part2() {
-    assert_eq!(195, solve_for(INPUT).1);
-    assert_eq!(440, solve_for(input::INPUT).1);
+    // assert_eq!(195, solve_for(INPUT).1);
+    // assert_eq!(440, solve_for(input::INPUT).1);
 }
